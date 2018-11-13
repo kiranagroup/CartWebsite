985f02fba3dd9ec074cdb6112853ec714ed7f1d9
Ojaswa Sharma <ojaswa1942@gmail.com>
2018-11-14 01:32:29 +0530

    Calculated price percentile for each Cluster, Category & Brand

--- src/assets/functions.js
+++ src/assets/functions.js
@@ -51,7 +51,7 @@ export const searchElastic = (esClient, index, body) =>{
     .catch(err => {console.log(err);})
 }
 
-export const requestCollections = async (esClient, index='learn4') =>{
+export const requestCollections = async (esClient, index='learn5') =>{
 	const body = {
 	    size: 0,
 		aggs: {
@@ -70,7 +70,13 @@ export const requestCollections = async (esClient, index='learn4') =>{
 							size: 1000
 						},
 						aggs: {
-							hits:  { top_hits: {size: 250} }
+							hits:  { top_hits: {size: 250} },
+							by_price: {
+								percentiles: {
+									field: "Price",
+									percents: [25,50,75,99.9]
+								}
+							}
 						}
 					},
 					by_brand: {
@@ -79,14 +85,25 @@ export const requestCollections = async (esClient, index='learn4') =>{
 							size: 1000
 						},
 						aggs: {
-							hits:  { top_hits: {size: 250} }
+							hits:  { top_hits: {size: 250} },
+							by_price: {
+								percentiles: {
+									field: "Price",
+									percents: [25,50,75,99.9]
+								}
+							}
+						}
+					},
+					by_price: {
+						percentiles: {
+							field: "Price",
+							percents: [25,50,75,99.9]
 						}
 					}
 				}
 			}
 		}
   	};
-	console.log('Check');
 	esClient.search({index: index, body: body})
 	.then(results => {
       console.log(results);
