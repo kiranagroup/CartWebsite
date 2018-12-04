6f52106bc63783b53baceb97113e67974cb3ecd9
Ojaswa Sharma <ojaswa1942@gmail.com>
2018-12-04 22:55:06 +0530

    SearchRes products listing

--- src/Components/SearchRes/SearchRes.js
+++ src/Components/SearchRes/SearchRes.js
@@ -1,6 +1,8 @@
 import React,{Component} from 'react';
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import {connect} from 'react-redux';
+import SearchProduct from './searchProduct';
+import Pane from '../searchPane/pane';
 
 const mapStateToProps = state => {
 	return{
@@ -11,42 +13,107 @@ const mapStateToProps = state => {
 
 class SearchRes extends Component {
 
-	componentWillMount(){
-		this.filteredProducts=[];
+    constructor(props){
+        super(props);
+        this.props=props;
+        this.gotData=false;
+        this.searchType='';
+        this.gotError=false;
+        this.error="";
+        this.products=[];
+        // this.count=50;
+        this.state={
+        	'count':50,
+        	products: []
+    	}
+    }
+
+    updateProducts = () =>{
+    	// this.setState({products: []});
 		switch(this.props.match.params.type){
-			case 'b0': console.log('This is b0');
+			case 'b0': this.setState({products: this.props.searchResults.aggregations.by_brand.buckets[0].by_top_hit.hits.hits});
 				break;
-			case 'b1': console.log('This is b1');
+			case 'b1': this.products.push(...this.props.searchResults.aggregations.by_brand.buckets[1].by_top_hit.hits.hits);
 				break;
-			case 'c0': console.log('This is c0');
+			case 'c0': this.products.push(...this.props.searchResults.aggregations.by_category.buckets[0].by_top_hit.hits.hits);
 				break;
-			case 'c1':
-				console.log('This is c1');
+			case 'c1': this.products.push(...this.props.searchResults.aggregations.by_category.buckets[1].by_top_hit.hits.hits);
 				break;
-			default: console.log('This is raw');
+			default: this.products.push(...this.props.searchResults.hits.hits);
+		}
+    }
+
+	componentDidMount(){
+		// let type=this.props.match.params.type;
+		// let resultRef=this.props.searchResults.by_brand.buckets.[0]
+		// if(type==='b0'){
+		// 	Object.keys(this.props.searchResults.by_brand)
+		// }
+		this.updateProducts();
+
+        if(this.products){
+            this.gotData=true;
+            this.gotError=false;
+            this.error='';
+        }
+        else{
+            this.gotError=true;
+            this.error="We are extremely sorry, but please try again later."
+        }
+        this.setState({});
+	}
+	componentDidUpdate(prevProps){
+		if(prevProps.searchResults!==this.props.searchResults){
+			this.setState({products: []});
+			this.products=[];
+			this.updateProducts();
+		console.log('Hi');
+
 		}
 	}
+	componentWillReceiveProps(newProps){
 
-	render(){
-		const {match, searchResults} = this.props;
-		console.log(this.props);
-		return(
-			<div>
-            <div className="row">
-	            <div id='searchPane' className="col-lg-2 col-md-3">
-	                <h3 align='center'>Search Pane</h3>
-	            </div>
-	            <div className="col-lg-10 col-md-9">
-{	              //  <Product items={this.products} counter={this.state.count}></Product>
-// 	            	                {show}}
-}
-				{`Hello. This is a query for ${this.props.match.params.query} of type ${this.props.match.params.type}`}
-	            </div>
-
-            </div>
-			</div>
-		)
 	}
+
+    render(){
+
+		const {match, searchResults} = this.props;
+		// return <div />
+        while(!this.gotData && !this.gotError){
+            return(
+                <div className="container centerIt">
+                    <img src={require('../../images/loader.gif')} className="load" alt="Loading..."/>
+                </div>
+            )
+        }
+        while(!this.gotData && this.gotError){
+            return(
+                <div className="container centerIt">
+                    <h4 className="alert">{this.error}</h4>
+                </div>
+            )
+        }
+        if(this.state.count<this.products.length){
+            var show= <div className="centerIt">       
+            <button className="btn" onClick={()=>{this.setState({'count':this.state.count+50});
+        }}> <b> Show More </b> </button>
+        </div>
+        }
+        else{
+            var show = <h4 className="centerIt mbott">That's all, Thank You.</h4>;
+        }
+        return(
+            <div className="row">
+            <div className="col-lg-2 col-md-2" style={{padding:'0 0 0 2px'}}>
+            <Pane category={this.category}></Pane>
+            </div>
+            <div className="col-lg-10 col-md-10">
+                <SearchProduct items={this.state.products} counter={this.state.count}></SearchProduct>
+                {show}
+            </div>
+            </div>
+        )
+    }
 }
 
 export default connect(mapStateToProps)(SearchRes)
---
+++ src/Components/SearchRes/searchProduct.js
@@ -0,0 +1,77 @@
+import React,{Component} from 'react';
+import '../Subscriptions/subs.css';
+import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Add from '../Product/Add';
+// import {Store} <f></f>rom '../../Models/Store';
+import Counter from '../Product/Counter';
+import {connect} from 'react-redux';
+
+class SearchProduct extends Component{
+    constructor(props){
+        super(props);
+        this.items=this.props.items;
+        // this.count=this.props.counter;
+        this.countCheck=0;
+        this.state={props:this.props};
+    }
+    refresh(){
+        this.setState({});
+    }
+    render(){
+        this.countCheck=0;
+        return(
+            <div className="row">
+                {this.items.map(Obj=>{
+                    if(this.countCheck===this.props.counter){
+                        return;
+                    }
+                    var button = <Add Obj={Obj._source} changed={this.refresh.bind(this)}></Add>;
+                    if(this.props.added){
+                    for(var i=0;i<this.props.added.length;i++){
+                            if(Obj._source["Product ID"]===this.props.added[i].Product["Product ID"]){
+                                button=<Counter Obj={Obj._source} quant={this.props.added[i].value} changed={this.refresh.bind(this)}></Counter> 
+                            }
+                        }
+                    }
+                    this.countCheck++;
+                    return(
+                        <div key={Obj._source["Product ID"]} className="col-lg-3 col-md-4 col-sm-6 col-6">
+                            <div className="box prod">
+                                <div className="imgbox">
+                                    <img src={Obj._source["Pic URL"]} alt=""/>
+                                </div>
+                                <div className="textbox prodtext">
+                                    <h5 className="gcol">{Obj._source.Brand}</h5>
+                                    <p className="extra">{Obj._source.Description}</p>
+                                    <p className="gcol">{Obj._source.Weight || 'Quantity'}</p>
+                                    <div className="row last">
+                                        <p className="col-sm-4 col-4 highlight">{Obj._source.Price || 'MRP'}</p>
+                                        <div className="col-sm-8 col-8 rightIt">
+                                            {button}
+                                        </div>
+                                    </div>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    )
+
+                })}
+            </div>
+        )
+    }
+}
+
+const mapStateToProps=(state)=>{
+    if(state.Reducer.added){
+        let curr={};
+        if(state.Reducer.current){
+            curr=state.Reducer.current;
+        }
+        let add = state.Reducer.added;
+        return {added:add,current:curr};
+    }
+    return {};
+}
+
+export default connect(mapStateToProps)(SearchProduct);
