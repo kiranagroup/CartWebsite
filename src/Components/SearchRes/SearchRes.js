8f91ebd614990be3b0261fc95511aad54c1b617a
Ojaswa Sharma <ojaswa1942@gmail.com>
2018-12-05 01:17:01 +0530

    Handled refresh exception

--- src/Components/SearchRes/SearchRes.js
+++ src/Components/SearchRes/SearchRes.js
@@ -3,11 +3,21 @@ import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import {connect} from 'react-redux';
 import SearchProduct from './searchProduct';
 import Pane from '../searchPane/pane';
+import {requestProducts} from '../../Models/actions';
+import elasticsearch from 'elasticsearch';
+import {elasticconfig} from '../../assets/service-act.js'
 
 const mapStateToProps = state => {
 	return{
 		searchField: state.searchProducts.searchField,
 		searchResults: state.requestProductsOnSearch.searchProductResults,
+		isPending: state.requestProductsOnSearch.isPending,
+		error: state.requestProductsOnSearch.error	
+	}
+}
+const mapDispatchToProps = dispatch =>{
+	return{
+		requestSearchResults: (esClient, searchBody) => requestProducts(esClient, searchBody),
 	}
 }
 
@@ -28,6 +38,53 @@ class SearchRes extends Component {
     	}
     }
 
+	// esClient = new elasticsearch.Client({
+	// 	host: elasticconfig.host,
+	// 	httpAuth: elasticconfig.httpAuth,
+	// 	log: 'error'
+	// });
+
+ //    searchBody = () => {return {
+	//     from: 0,
+	//     query: {
+	//     	bool:{
+	//     		should:{
+	// 				multi_match: {
+	// 					fields: ['Brand', 'Category', 'Description'],
+	// 					query: this.props.match.params.query,
+	// 					fuzziness: '2',
+	// 				}
+	// 			}
+	// 		}
+	//     },
+	// 	aggs: {
+	// 		by_brand: {
+	// 			terms: {
+	// 				field: 'Brand.keyword',
+	// 				size: 10,
+	// 				order: { max_score: 'desc' }
+	// 			},
+	// 			aggs: {
+	// 				by_top_hit: { top_hits: {size: 15} },
+
+	// 				max_score: {max: { script: "_score" } }
+	// 			}
+	// 		},
+	// 		by_category: {
+	// 			terms: {
+	// 				field: 'Category.keyword',
+	// 				size: 10,
+	// 				order: { max_score: 'desc' }
+	// 			},
+	// 			aggs: {
+	// 				by_top_hit: { top_hits: {size: 15} },
+
+	// 				max_score: {max: { script: "_score" } }
+	// 			}
+	// 		}
+	// 	}
+ //  	}};
+
     updateProducts = () =>{
 		switch(this.props.match.params.type){
 			case 'b0': this.setState({products: this.props.searchResults.aggregations.by_brand.buckets[0].by_top_hit.hits.hits});
@@ -43,7 +100,11 @@ class SearchRes extends Component {
     }
 
 	componentDidMount(){
-		this.updateProducts();
+    	if(Object.keys(this.props.searchResults).length === 0){
+			// this.props.requestSearchResults(this.esClient, this.searchBody());	
+			this.props.history.push('/');
+    	} else
+			this.updateProducts();
         if(this.state.products){
             this.gotData=true;
             this.gotError=false;
@@ -53,15 +114,16 @@ class SearchRes extends Component {
             this.gotError=true;
             this.error="We are extremely sorry, but please try again later."
         }
-        this.setState({});
 	}
 
-	componentWillReceiveProps(newProps){
-		if(newProps.match.params.type !== this.props.match.params.type
-			|| newProps.match.params.query !== this.props.match.params.query){
+	componentDidUpdate(oldProps){
+		if(oldProps.match.params.type !== this.props.match.params.type
+			|| oldProps.match.params.query !== this.props.match.params.query
+			|| oldProps.searchResults!==this.props.searchResults){
 	    	this.updateProducts();
 		}
 	}
+
 
     render(){
 
@@ -104,4 +166,4 @@ class SearchRes extends Component {
     }
 }
 
-export default connect(mapStateToProps)(SearchRes)
+export default connect(mapStateToProps, mapDispatchToProps)(SearchRes)
